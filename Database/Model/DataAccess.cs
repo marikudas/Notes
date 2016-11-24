3962eb273ec65aa2f79459827c653e0ea18dcdbc
Mari <Mari@DESKTOP-ON63OMU>
2016-11-24 19:15:12 +0200

    create db

---
+++ Database/Model/DataAccess.cs
@@ -0,0 +1,75 @@
+﻿using Database.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Database
+{
+    class DataAccess
+    {
+        public void AddUser(User user)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Users.Add(user);
+            }
+        }
+
+        public void AddNotebook(Notebook notebook)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Notebooks.Add(notebook);
+            }
+        }
+
+        public void AddNote(Note note)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Notes.Add(note);
+            }
+        }
+
+        public void RemoveUser(User user)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Users.Remove(user);
+            }
+        }
+
+        public void RemoveNotebook(Notebook notebook)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Notebooks.Remove(notebook);
+            }
+        }
+
+        public void RemoveNote(Note note)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Notes.Remove(note);
+            }
+        }
+
+        public void RemoveUser(int userId)
+        {
+            using (Entities db = new Entities())
+            {
+                User user = db.Users.FirstOrDefault(u => u.Id == userId);
+                if (user != null)
+                    db.Users.Remove(user);
+            }
+        }
+
+        public List<User> GetUsers()
+        {
+            using (Entities db = new Entities())
+            {
+                return db.Users.ToList();
+            }
+        }
+    }
+}
--- Database/Modell/Model.edmx.sql
+++ Database/Modell/Model.edmx.sql
@@ -2,13 +2,13 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 11/24/2016 18:44:59
--- Generated from EDMX file: D:\TFS\Coursework (Notes)\Notes\Notes\Database\Model\Model.edmx
+-- Date Created: 10/27/2016 19:07:24
+-- Generated from EDMX file: C:\Users\itfak\Documents\Visual Studio 2015\Projects\Presentation\Database\Model\Model.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
 GO
-USE [D:\TFS\COURSEWORK (NOTES)\NOTES\NOTES\DATABASE\NOTEDB.MDF];
+USE [NoteDb];
 GO
 IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
 GO
@@ -17,30 +17,26 @@ GO
 -- Dropping existing FOREIGN KEY constraints
 -- --------------------------------------------------
 
-IF OBJECT_ID(N'[dbo].[FK_NotebookNote]', 'F') IS NOT NULL
-    ALTER TABLE [dbo].[Notes] DROP CONSTRAINT [FK_NotebookNote];
-GO
-IF OBJECT_ID(N'[dbo].[FK_UserNotebook]', 'F') IS NOT NULL
-    ALTER TABLE [dbo].[Notebooks] DROP CONSTRAINT [FK_UserNotebook];
-GO
 
 -- --------------------------------------------------
 -- Dropping existing tables
 -- --------------------------------------------------
 
-IF OBJECT_ID(N'[dbo].[Notebooks]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[Notebooks];
-GO
-IF OBJECT_ID(N'[dbo].[Notes]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[Notes];
-GO
-IF OBJECT_ID(N'[dbo].[Users]', 'U') IS NOT NULL
-    DROP TABLE [dbo].[Users];
-GO
 
 -- --------------------------------------------------
 -- Creating all tables
 -- --------------------------------------------------
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL,
+    [Email] nvarchar(max)  NOT NULL,
+    [Login] nvarchar(max)  NOT NULL,
+    [Password] nvarchar(max)  NOT NULL
+);
+GO
 
 -- Creating table 'Notebooks'
 CREATE TABLE [dbo].[Notebooks] (
@@ -64,20 +60,15 @@ CREATE TABLE [dbo].[Notes] (
 );
 GO
 
--- Creating table 'Users'
-CREATE TABLE [dbo].[Users] (
-    [Id] int IDENTITY(1,1) NOT NULL,
-    [FirstName] nvarchar(max)  NOT NULL,
-    [LastName] nvarchar(max)  NOT NULL,
-    [Email] nvarchar(max)  NOT NULL,
-    [Login] nvarchar(max)  NOT NULL,
-    [Password] nvarchar(max)  NOT NULL
-);
-GO
-
 -- --------------------------------------------------
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
 
 -- Creating primary key on [Id] in table 'Notebooks'
 ALTER TABLE [dbo].[Notebooks]
@@ -91,30 +82,9 @@ ADD CONSTRAINT [PK_Notes]
     PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
--- Creating primary key on [Id] in table 'Users'
-ALTER TABLE [dbo].[Users]
-ADD CONSTRAINT [PK_Users]
-    PRIMARY KEY CLUSTERED ([Id] ASC);
-GO
-
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
-
--- Creating foreign key on [NotebookId] in table 'Notes'
-ALTER TABLE [dbo].[Notes]
-ADD CONSTRAINT [FK_NotebookNote]
-    FOREIGN KEY ([NotebookId])
-    REFERENCES [dbo].[Notebooks]
-        ([Id])
-    ON DELETE CASCADE ON UPDATE NO ACTION;
-GO
-
--- Creating non-clustered index for FOREIGN KEY 'FK_NotebookNote'
-CREATE INDEX [IX_FK_NotebookNote]
-ON [dbo].[Notes]
-    ([NotebookId]);
-GO
 
 -- Creating foreign key on [UserId] in table 'Notebooks'
 ALTER TABLE [dbo].[Notebooks]
@@ -122,13 +92,28 @@ ADD CONSTRAINT [FK_UserNotebook]
     FOREIGN KEY ([UserId])
     REFERENCES [dbo].[Users]
         ([Id])
-    ON DELETE CASCADE ON UPDATE NO ACTION;
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_UserNotebook'
 CREATE INDEX [IX_FK_UserNotebook]
 ON [dbo].[Notebooks]
     ([UserId]);
+GO
+
+-- Creating foreign key on [NotebookId] in table 'Notes'
+ALTER TABLE [dbo].[Notes]
+ADD CONSTRAINT [FK_NotebookNote]
+    FOREIGN KEY ([NotebookId])
+    REFERENCES [dbo].[Notebooks]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_NotebookNote'
+CREATE INDEX [IX_FK_NotebookNote]
+ON [dbo].[Notes]
+    ([NotebookId]);
 GO
 
 -- --------------------------------------------------
